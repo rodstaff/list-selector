af6dc4b4631b792ae133f398ab7837af90ace8a8
rodk <rod2010rod2010@hotmail.com>
2016-11-02 20:57:28 -0400

    first commit

---
+++ webpack.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+  entry: './',  
+  devtool:  'inline-source-map',
+  devServer: {
+  	publicPath: '/',   
+  	filename: 'bundle.js',  
+  	host: '0.0.0.0',
+  	port: 8085
+  },
+    module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader:  'babel-loader',
+        query: {
+          presets: ['react', 'es2015']
+        }
+      }
+    ]
+  }
+}
