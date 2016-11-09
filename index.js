f0e47de2c49f666d1a5d07307b1a90f52176ed94
rodk <rod2010rod2010@hotmail.com>
2016-11-09 07:38:04 -0500

    created ES6 version of code

--- index.js
+++ index.js
@@ -1,9 +1,37 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-var FilteredList = React.createClass({
-  getInitialState: function() {
-    return {
+class FilteredList extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      initialItems: this.props.initialItems,
+      items: this.props.items
+    }
+  }
+  filterList(e) {
+    var updatedList = this.state.initialItems.filter((item) => {
+      return (
+        item.toLowerCase().search(e.target.value.toLowerCase()) !== -1
+      );
+    });
+    this.setState({
+      items: updatedList
+    });
+  }
+  render() {
+    return (
+      <div>
+        <h3>Filter your list: </h3>
+        <input type="text" onChange={this.filterList.bind(this)} 
+          placeholder="type here"
+        /> &nbsp;INPUT
+        <List items={this.state.items} />
+      </div>
+    );
+  }
+}
+FilteredList.defaultProps = {
       initialItems: [
          "Apple | Pyrus malus",
          "Bamboo | Bamboosa aridinarifolia",
@@ -41,47 +69,43 @@ var FilteredList = React.createClass({
          "Wheat | Triticum Aestivum"
       ],
       items: []
-    };
-  },
-  filterList: function(ev) {
-    var updatedList = this.state.initialItems.filter(function(item) {
-      return (
-        item.toLowerCase().search(
-          ev.target.value.toLowerCase()) !== -1
-      );
-    });
-    this.setState({
-      items: updatedList
-    });
-  },
-  componentWillMount:  function() {
-    this.setState({
-      items: this.state.initialItems
-    });
-  },
-  render: function() {
+}
+
+// class List extends React.Component {
+//   render() {
+//     var listItems = this.props.items.map((item) => {
+//       return (<li key={item}>{item}</li>);
+//     })
+//     return (
+//       <div>
+//         <h3>List</h3>
+//         <ul>{listItems}</ul>
+//       </div>
+//     );
+//   }
+// }
+class List extends React.Component {
+  render() {
     return (
-      <div className="filter-list">
-        <input type="text" placeholder="Type and search here" onChange={this.filterList} /> INPUT
-        <List items={this.state.items} />
+      <div>
+        <h3>List</h3>
+        <ul>
+         {this.props.items.map((item) => {
+           return (
+             <li key={item}>{item}</li>
+           );
+         })}
+        </ul>
       </div>
     );
   }
-});
-
-var List = React.createClass({
-  render: function() {
-    return (
-      <ul>
-        {this.props.items.map(function(item) {
-          return (
-            <li key={item}>{item}</li>
-          );
-        }) }
-      </ul>
-    );
-  }
-});
+}
+List.propTypes = {
+  items: React.PropTypes.array.isRequired
+}
+// List.defaultProps = {
+//   items: []
+// }
 
 ReactDOM.render(
   <FilteredList/>, document.getElementById('root')
